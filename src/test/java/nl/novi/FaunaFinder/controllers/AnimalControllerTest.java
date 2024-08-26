64c91cfbe4a7827970ba2ac6a1a575a54076794a
BeheadedBadger <ZarradeRuijter@gmail.com>
2024-08-26 15:14:57 +0200

    2 tests for AnimalController
    Tests written for creating animals and retrieving animals from the repository.

--- src/test/java/nl/novi/FaunaFinder/controllers/AnimalControllerTest.java
+++ src/test/java/nl/novi/FaunaFinder/controllers/AnimalControllerTest.java
@@ -1,34 +1,36 @@
 package nl.novi.FaunaFinder.controllers;
 
 import jakarta.persistence.*;
+import nl.novi.FaunaFinder.dtos.input.AnimalInputDto;
+import nl.novi.FaunaFinder.dtos.input.UserInputDto;
+import nl.novi.FaunaFinder.dtos.mapper.AnimalMapper;
+import nl.novi.FaunaFinder.dtos.mapper.UserMapper;
+import nl.novi.FaunaFinder.dtos.output.AnimalOutputDto;
+import nl.novi.FaunaFinder.dtos.output.AuthenticationResponse;
 import nl.novi.FaunaFinder.models.Animal;
 import nl.novi.FaunaFinder.models.Role;
+import nl.novi.FaunaFinder.models.Sex;
 import nl.novi.FaunaFinder.models.User;
 import nl.novi.FaunaFinder.repositories.AnimalRepository;
 import nl.novi.FaunaFinder.repositories.UserRepository;
 import nl.novi.FaunaFinder.service.AnimalService;
+import nl.novi.FaunaFinder.service.AuthenticationService;
+import nl.novi.FaunaFinder.service.UserService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 class AnimalControllerTest {
     @Mock
-    @InjectMocks
     AnimalRepository animalRepository;
-    @Mock
-    @InjectMocks
-    UserRepository userRepository;
-    @Mock
     @InjectMocks
     AnimalService animalService;
 
@@ -37,43 +39,7 @@ class AnimalControllerTest {
 
     @BeforeEach
     void setUp() {
-        User fakeTestShelter = new User();
-        fakeTestShelter.setRole(Role.SHELTER);
-        fakeTestShelter.setUsername("fakeTestShelter");
-        fakeTestShelter.setPassword("Password");
-
-        User fakeTestUser = new User();
-        fakeTestUser.setRole(Role.USER);
-        fakeTestUser.setUsername("fakeTestUser");
-        fakeTestUser.setPassword("Password");
-
-        List<User> favs = new ArrayList<>();
-        favs.add(fakeTestUser);
-
-        Animal cat = new Animal();
-        cat.setName("Mittens");
-        cat.setAge(1);
-        cat.setSex(Animal.Sex.X);
-        cat.setCommonSpeciesName("cat");
-        cat.setScientificSpeciesName("Felis Cattus");
-        cat.setSpeciesCategory("Mammals");
-        cat.setWarning(false);
-        cat.setDescription("Mittens is a very friendly cat");
-        cat.setShelter(fakeTestShelter);
-        cat.setFavourites(favs);
-
-        Animal ferret = new Animal();
-        ferret.setName("Freddy");
-        ferret.setAge(2);
-        ferret.setSex(Animal.Sex.M);
-        ferret.setCommonSpeciesName("Ferret");
-        ferret.setScientificSpeciesName("Mustela Furo");
-        ferret.setSpeciesCategory("Mammals");
-        ferret.setWarning(true);
-        ferret.setWarningExplanation("Freddy can and will bite. Experienced owners only");
-        ferret.setDescription("Freddy is a lovely ferret, but can play bite. He'll make a lovely pet for someone who knows how to handle him");
-        ferret.setShelter(fakeTestShelter);
-        ferret.setFavourites(favs);
+        MockitoAnnotations.openMocks(this);
     }
 
     @AfterEach
@@ -82,11 +48,78 @@ class AnimalControllerTest {
 
     @Test
     @DisplayName("Submit a new animal")
-    void addAnimal() {
+    void addAnimal() throws Exception {
+        //Arrange
+        AnimalInputDto ferret = new AnimalInputDto();
+        ferret.name = "Freddy";
+        ferret.age = 3;
+        ferret.sex = Sex.M;
+        ferret.commonSpeciesName = "Ferret";
+        ferret.scientificSpeciesName = "Mustela Furo";
+        ferret.warning = true;
+        ferret.warningExplanation = "Can and will bite";
+        ferret.description = "Freddy is a lovely ferret, but can play bite. He'll make a lovely pet for someone who knows how to handle him";
+        ferret.category = "Mammals";
+
+        AnimalOutputDto created = animalService.create(ferret);
+        Animal newFerret = AnimalMapper.fromInputDtoToModel(ferret);
+        animalRepository.save(newFerret);
+
+        //Act
+        List<Animal> found = animalRepository.findAll();
+
+        //Assert
+        for (Animal animal : found)
+        {
+            if (Objects.equals(animal.getName(), "Freddy")) {
+                assertEquals(created.getCategory(), animal.getSpeciesCategory());
+                assertEquals(created.getCommonSpeciesName(), animal.getCommonSpeciesName());
+                assertEquals(created.getAge(), animal.getAge());
+            }
+        }
     }
 
     @Test
-    @DisplayName("")
-    void getAnimal() {
+    @DisplayName("Get a specific animal")
+    void getAnimal() throws Exception {
+        //Arrange
+        AnimalInputDto ferret = new AnimalInputDto();
+        ferret.name = "Freddy";
+        ferret.age = 3;
+        ferret.sex = Sex.M;
+        ferret.commonSpeciesName = "Ferret";
+        ferret.scientificSpeciesName = "Mustela Furo";
+        ferret.warning = true;
+        ferret.warningExplanation = "Can and will bite";
+        ferret.description = "Freddy is a lovely ferret, but can play bite. He'll make a lovely pet for someone who knows how to handle him";
+        ferret.category = "Mammals";
+
+        Animal newFerret = AnimalMapper.fromInputDtoToModel(ferret);
+        animalRepository.save(newFerret);
+
+        AnimalInputDto cat = new AnimalInputDto();
+        cat.name = "Mittens";
+        cat.age = 1;
+        cat.sex = Sex.X;
+        cat.commonSpeciesName = "Cat";
+        cat.scientificSpeciesName = "Felis Cattus";
+        cat.category = "Mammals";
+        cat.warning = false;
+        cat.description = "Mittens is a very friendly cat";
+
+        Animal newCat = AnimalMapper.fromInputDtoToModel(ferret);
+        animalRepository.save(newCat);
+
+        //Act
+        List<Animal> found = animalRepository.findAll();
+
+        //Assert
+        for (Animal animal : found) {
+            if (Objects.equals(animal.getName(), "Mittens")) {
+                assertEquals("Cat", animal.getCommonSpeciesName());
+                assertEquals(1, animal.getAge());
+                assertEquals("Mammals", animal.getSpeciesCategory());
+            }
+        }
     }
 }
